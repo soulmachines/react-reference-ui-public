67ad1e5536c33cb3cfad8d67f481e0f06f999c51
Jade Fung <jade@fungj.net>
2021-07-21 18:06:07 -0700

    removed logging

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -9,59 +9,57 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { headerHeight, landingBackground } from '../config';
 
-const Landing = ({ className }) => {
-  console.log('hi');
-  return (
-    <div className={className}>
-      <Header />
-      <div className="landing-wrapper">
-        <div className="container">
-          <div className="card col-lg-6 p-3">
-            <div className="card-body">
-              <h2 className="first-things-first">
-                <CheckSquare />
+const Landing = ({ className }) => (
+  <div className={className}>
+    <Header />
+    <div className="landing-wrapper">
+      <div className="container">
+        <div className="card col-lg-6 p-3">
+          <div className="card-body">
+            <h2 className="first-things-first">
+              <CheckSquare />
+              {' '}
+              First things first
+            </h2>
+
+            <b>Before we get started:</b>
+            <br />
+            By proceeding you acknowledge and accept the following terms:
+
+            <div className="mb-4">
+              <div className="mx-4 mt-2">
+                <FileEarmarkMedical />
                 {' '}
-                First things first
-              </h2>
-
-              <b>Before we get started:</b>
-              <br />
-              By proceeding you acknowledge and accept the following terms:
-
-              <div className="mb-4">
-                <div className="mx-4 mt-2">
-                  <FileEarmarkMedical />
-                  {' '}
-                  <a href="https://www.soulmachines.com/privacy-policy/" target="_blank" rel="noreferrer">Privacy Policy</a>
-                </div>
-                <div className="mx-4">
-                  <FileEarmarkMedical />
-                  {' '}
-                  <a href="https://www.soulmachines.com/privacy-policy/" target="_blank" rel="noreferrer">EULA</a>
-                </div>
-                <div className="mx-4">
-                  <i className="bi bi-camera-video" />
-                  <CameraVideo />
-                  {' '}
-                  My camera and microphone will be used
-                </div>
-                <div className="mx-4">
-                  <GraphUp />
-                  {' '}
-                  Anonymized usage data will be captured
-                </div>
+                <a href="https://www.soulmachines.com/privacy-policy/" target="_blank" rel="noreferrer">Privacy Policy</a>
               </div>
-              <strong>Frequently Asked Questions:</strong>
+              <div className="mx-4">
+                <FileEarmarkMedical />
+                {' '}
+                <a href="https://www.soulmachines.com/privacy-policy/" target="_blank" rel="noreferrer">EULA</a>
+              </div>
+              <div className="mx-4">
+                <i className="bi bi-camera-video" />
+                <CameraVideo />
+                {' '}
+                My camera and microphone will be used
+                </div>
+              <div className="mx-4">
+                <GraphUp />
+                {' '}
+                Anonymized usage data will be captured
+                </div>
+            </div>
+            <strong>Frequently Asked Questions:</strong>
 
-              <div className="accordion accordion-flush" id="accordionFlushExample">
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="flush-headingOne">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingOne">
+                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                       Who is running this system?
                     </button>
-                  </h2>
-                  <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div className="accordion-body">
+                </h2>
+                <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                  <div className="accordion-body">
                       <p>
                         Soul Machines Limited is a New Zealand company whose address is
                         L1, 106 Customs Street West, Auckland, 1010, New Zealand.
@@ -71,16 +69,16 @@ const Landing = ({ className }) => {
                         “Embodied Cognitive User Experience” which we call a “Digital Person”.
                       </p>
                     </div>
-                  </div>
                 </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="flush-headingTwo">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingTwo">
+                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                       What information are we collecting?
                     </button>
-                  </h2>
-                  <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                    <div className="accordion-body">
+                </h2>
+                <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                  <div className="accordion-body">
                       <p>
                         For the Digital Person to be able to interact with you in an authentic and
                         human way, their Digital Brain™ needs to collect and process information
@@ -93,16 +91,16 @@ const Landing = ({ className }) => {
                         generate the best experience possible.
                       </p>
                     </div>
-                  </div>
                 </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="flush-headingThree">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingThree">
+                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
                       Why are we collecting this information?
                     </button>
-                  </h2>
-                  <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                    <div className="accordion-body">
+                </h2>
+                <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                  <div className="accordion-body">
                       <p>
                         We want your experience with the Digital Person to be as fun, natural and
                         engaging as possible. You can help us do this by allowing the Digital
@@ -121,24 +119,23 @@ const Landing = ({ className }) => {
                         privacy practices and regulations. We take your privacy very seriously.
                       </p>
                     </div>
-                  </div>
                 </div>
               </div>
             </div>
-            <Link
-              to="/loading"
-              className="btn btn-success btn-lg action-btn"
-            >
-              I accept terms and conditions
-              {' '}
-              <ArrowRightCircleFill />
-            </Link>
           </div>
+          <Link
+            to="/loading"
+            className="btn btn-success btn-lg action-btn"
+          >
+            I accept terms and conditions
+            {' '}
+            <ArrowRightCircleFill />
+          </Link>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 Landing.propTypes = {
   className: PropTypes.string.isRequired,
