d172b9fedba28d3e375e31cb3defdee7d109cbd2
Jade Fung <jade@fungj.net>
2021-07-26 15:00:03 -0700

    eslint autofix

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -42,12 +42,12 @@ const Landing = ({ className }) => (
                 <CameraVideo />
                 {' '}
                 My camera and microphone will be used
-                </div>
+              </div>
               <div className="mx-4">
                 <GraphUp />
                 {' '}
                 Anonymized usage data will be captured
-                </div>
+              </div>
             </div>
             <strong>Frequently Asked Questions:</strong>
 
@@ -55,70 +55,70 @@ const Landing = ({ className }) => (
               <div className="accordion-item">
                 <h2 className="accordion-header" id="flush-headingOne">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                      Who is running this system?
-                    </button>
+                    Who is running this system?
+                  </button>
                 </h2>
                 <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                   <div className="accordion-body">
-                      <p>
-                        Soul Machines Limited is a New Zealand company whose address is
-                        L1, 106 Customs Street West, Auckland, 1010, New Zealand.
-                      </p>
-                      <p>
-                        Soul Machines have built a “Digital Brain” and
-                        “Embodied Cognitive User Experience” which we call a “Digital Person”.
-                      </p>
-                    </div>
+                    <p>
+                      Soul Machines Limited is a New Zealand company whose address is
+                      L1, 106 Customs Street West, Auckland, 1010, New Zealand.
+                    </p>
+                    <p>
+                      Soul Machines have built a “Digital Brain” and
+                      “Embodied Cognitive User Experience” which we call a “Digital Person”.
+                    </p>
+                  </div>
                 </div>
               </div>
               <div className="accordion-item">
                 <h2 className="accordion-header" id="flush-headingTwo">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                      What information are we collecting?
-                    </button>
+                    What information are we collecting?
+                  </button>
                 </h2>
                 <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                   <div className="accordion-body">
-                      <p>
-                        For the Digital Person to be able to interact with you in an authentic and
-                        human way, their Digital Brain™ needs to collect and process information
-                        about your expressions.
-                      </p>
-                      <p>
-                        As soon as it is collected, this information is anonymized. We do not
-                        keep this information, provide it to third parties, or share it.
-                        It is purely used while you are interacting with the Digital Person to
-                        generate the best experience possible.
-                      </p>
-                    </div>
+                    <p>
+                      For the Digital Person to be able to interact with you in an authentic and
+                      human way, their Digital Brain™ needs to collect and process information
+                      about your expressions.
+                    </p>
+                    <p>
+                      As soon as it is collected, this information is anonymized. We do not
+                      keep this information, provide it to third parties, or share it.
+                      It is purely used while you are interacting with the Digital Person to
+                      generate the best experience possible.
+                    </p>
+                  </div>
                 </div>
               </div>
               <div className="accordion-item">
                 <h2 className="accordion-header" id="flush-headingThree">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                      Why are we collecting this information?
-                    </button>
+                    Why are we collecting this information?
+                  </button>
                 </h2>
                 <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                   <div className="accordion-body">
-                      <p>
-                        We want your experience with the Digital Person to be as fun, natural and
-                        engaging as possible. You can help us do this by allowing the Digital
-                        Person to study and respond to your expressions in order to interpret
-                        your emotional response so that they can provide an appropriate answer.
-                        It’s just like a conversation with a real person where they are not just
-                        interpreting what you say, but how you say it.
-                      </p>
-                      <p>
-                        In the process, you are helping advance technology and experiencing the
-                        future of human-to-machine interaction.
-                      </p>
+                    <p>
+                      We want your experience with the Digital Person to be as fun, natural and
+                      engaging as possible. You can help us do this by allowing the Digital
+                      Person to study and respond to your expressions in order to interpret
+                      your emotional response so that they can provide an appropriate answer.
+                      It’s just like a conversation with a real person where they are not just
+                      interpreting what you say, but how you say it.
+                    </p>
+                    <p>
+                      In the process, you are helping advance technology and experiencing the
+                      future of human-to-machine interaction.
+                    </p>
 
-                      <p>
-                        All conversation data collected is anonymized and complies with current
-                        privacy practices and regulations. We take your privacy very seriously.
-                      </p>
-                    </div>
+                    <p>
+                      All conversation data collected is anonymized and complies with current
+                      privacy practices and regulations. We take your privacy very seriously.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
