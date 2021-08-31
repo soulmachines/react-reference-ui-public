8977cf1a056261ee62b73a884cc35999a2287fec
Jade Fung <jade@fungj.net>
2021-08-31 08:53:37 -0700

    fixed proptype obj, removed ID arg

--- src/components/ContentCards/Image.js
+++ src/components/ContentCards/Image.js
@@ -2,19 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Image = ({ data }) => {
-  const { url, alt, id } = data;
+  const { url, alt } = data;
   return (
     <div style={{ maxWidth: '100%' }}>
-      <img src={url} alt={alt} style={{ width: 'auto', height: 'auto' }} data-sm-content={id} />
+      <img src={url} alt={alt} style={{ width: 'auto', height: 'auto' }} />
     </div>
   );
 };
 
 Image.propTypes = {
-  data: PropTypes.objectOf({
+  data: PropTypes.shape({
     url: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
   }).isRequired,
 };
 
