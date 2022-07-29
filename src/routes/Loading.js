d48de09e4b159065fa9f17bac2f73d2efe753f93
Jade Fung <jade@fungj.net>
2022-07-29 16:20:30 -0700

    removed unused var

--- src/routes/Loading.js
+++ src/routes/Loading.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { ArrowLeftCircleFill, ArrowRightCircleFill, MicFill } from 'react-bootstrap-icons';
 import { createScene } from '../store/sm';
