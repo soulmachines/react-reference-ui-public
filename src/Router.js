722f8253e8842c70c5c7d88066ca53a0175f73f3
Jade Fung <jade@fungj.net>
2022-08-19 13:00:31 -0700

    moved GA to router file

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,29 @@
+import ReactGA from 'react-ga';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  withRouter,
 } from 'react-router-dom';
 import DPChat from './routes/DPChat';
 import Landing from './routes/Landing';
 import Loading from './routes/Loading';
 import Feedback from './routes/Feedback';
+
+// only init google analytics if a tracking ID is defined in env
+const { REACT_APP_GA_TRACKING_ID } = process.env;
+if (REACT_APP_GA_TRACKING_ID) ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+
+// make GA aware of what pages people navigate to in react router
+const LinkGAtoRouter = withRouter(({ history }) => {
+  history.listen((location) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  });
+  return <div />;
+});
 
 function App() {
   return (
@@ -28,6 +43,7 @@ function App() {
           <Landing />
         </Route>
       </Switch>
+      <LinkGAtoRouter />
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import ReactGA from 'react-ga';
 import ReactTooltip from 'react-tooltip';
-import { withRouter } from 'react-router-dom';
 import Router from './Router';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './globalStyle';
-
-const { REACT_APP_GA_TRACKING_ID } = process.env;
-
-let bindGAtoRouter = null;
-
-// only init google analytics if a tracking ID is defined in env
-if (REACT_APP_GA_TRACKING_ID) {
-  // init GA tracking
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
-  // make GA aware of what pages people navigate to in react router
-  bindGAtoRouter = withRouter(({ history }) => {
-    history.listen((location) => {
-      ReactGA.set({ page: location.pathname });
-      ReactGA.pageview(location.pathname);
-    });
-    return <div />;
-  });
-}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -36,7 +16,6 @@ ReactDOM.render(
     {/* globally enable react tooltips */}
     <ReactTooltip />
     {/* will be null if GA tracking is not enabled */}
-    {bindGAtoRouter}
   </React.StrictMode>,
   document.getElementById('root'),
 );
