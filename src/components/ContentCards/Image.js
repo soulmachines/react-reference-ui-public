228a528e50cc0d4048eca54cf10e99f9a2492e83
Scott Prince <79943542+SM-scottprince@users.noreply.github.com>
2022-10-25 13:59:20 -0700

    Initialize Rel

---
+++ src/components/ContentCards/Image.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function Image({ data, className }) {
+  const { url, alt, caption } = data;
+  return (
+    <div className={className}>
+      <div>
+        <img src={url} alt={alt} style={{ width: '100%', height: 'auto' }} />
+        {
+        caption ? <div className="text-center p-2">{caption}</div> : null
+      }
+      </div>
+    </div>
+  );
+}
+
+Image.propTypes = {
+  data: PropTypes.shape({
+    url: PropTypes.string,
+    alt: PropTypes.string,
+    caption: PropTypes.string,
+  }).isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+export default styled(Image)`
+  border-radius: 10px;
+  border: 1px solid rgba(0,0,0,0.2);
+  overflow: hidden;
+
+  background: #393939;
+  color: #FFF;
+`;
